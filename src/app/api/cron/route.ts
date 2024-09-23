7082026a6f5943133527a468eb9d6dc054762d9f
Ahmed Kazez <ahmedgazez@gmail.com>
2024-09-23 16:38:09 +0100

    Fixed build bug

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,7 @@
 import cleanOrphanFiles from '@/actions/files';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   const res = await cleanOrphanFiles();
   return NextResponse.json(res);
 }
