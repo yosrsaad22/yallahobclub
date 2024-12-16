40f7bf58a945ed9c9ba53beb8ab7181806cfc9c3
Ahmed Kazez <ahmedgazez@gmail.com>
2024-12-16 12:28:41 +0100

    Changed dashboard bg and minor ui colors

--- src/components/full-course/full-course.tsx
+++ src/components/full-course/full-course.tsx
@@ -241,7 +241,7 @@ export const FullCourseComponent = ({ chapters }: FullCourseComponentProps) => {
       {/* course content section */}
 
       <div className="relative flex flex-col items-center justify-center py-16">
-        <div className="absolute inset-0 mt-12 w-full rounded-full bg-gradient-to-r from-primary/60 to-secondary/60 opacity-30 blur-3xl"></div>
+        <div className="absolute inset-0 mt-12 w-full rounded-full bg-gradient-to-r from-primary/60 to-secondary/70 opacity-30 blur-3xl"></div>
 
         <div className="relative flex h-8 flex-col items-center justify-center">
           <div className="absolute inset-1 rounded-full bg-gradient-to-r from-primary/60 to-secondary/60 opacity-50 blur-lg"></div>
@@ -255,18 +255,19 @@ export const FullCourseComponent = ({ chapters }: FullCourseComponentProps) => {
           </span>
         </h1>
         <p className="text-md pt-8 text-center md:max-w-4xl ">{t('course-content')}</p>
-        <div className=" feature-glass-gradient relative mt-12 flex w-full min-w-[80%] max-w-full items-end justify-center rounded-xl border border-slate-700/70  backdrop-blur-sm md:max-w-[80%]">
-          <div className="absolute inset-8 rounded-full bg-gradient-to-r from-primary/40 to-secondary/80 opacity-40 blur-3xl"></div>
-          <div className="flex h-full w-full  flex-col gap-6 p-6 ">
-            {chaptersWithDuration.map((chapter, index) => (
-              <React.Fragment key={index}>
-                <p className="text-md flex justify-between text-foreground/90">
-                  {locale === localeOptions.FR ? chapter.title_fr : chapter.title_en}
-                  <span className="text-md pl-4 font-semibold text-slate-400">{chapter.duration} min</span>
-                </p>
-                {index < chaptersWithDuration.length - 1 && <hr className="my-1 w-full border-slate-700" />}
-              </React.Fragment>
-            ))}
+        <div className=" feature-glass-gradient custom-scrollbar relative mt-12 flex w-full min-w-[80%] max-w-full items-end justify-center overflow-y-auto rounded-md border border-slate-700/70 bg-background/20  backdrop-blur-sm md:max-w-[80%]">
+          <div className="flex h-full max-h-[20rem] w-full flex-col">
+            <div className=" flex w-full flex-col gap-6 p-6">
+              {chaptersWithDuration.map((chapter, index) => (
+                <React.Fragment key={index}>
+                  <p className="text-md flex justify-between text-foreground/90">
+                    {locale === localeOptions.FR ? chapter.title_fr : chapter.title_en}
+                    <span className="text-md pl-4 font-semibold text-slate-400">{chapter.duration} min</span>
+                  </p>
+                  {index < chaptersWithDuration.length - 1 && <hr className="my-1 w-full border-slate-700" />}
+                </React.Fragment>
+              ))}
+            </div>
           </div>
         </div>
       </div>
