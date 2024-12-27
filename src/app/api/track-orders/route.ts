ab0e5fee1c3fc23c6e131b3f5851c62426de0fa6
Ahmed Kazez <ahmedgazez@gmail.com>
2024-12-27 15:22:48 +0100

    Tracking secret debug

--- src/app/api/track-orders/route.ts
+++ src/app/api/track-orders/route.ts
@@ -2,6 +2,7 @@ import { trackOrders } from '@/actions/orders';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
+  console.log(req.headers.get('Authorization'));
   if (req.headers.get('Authorization') !== `Bearer ${process.env.TRACKING_SECRET}`) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
